2e6dba8f202dfad30c247c42c4a43c72f580e0c2
Lyndsey Bonelli <lyndsey.bonelli@zuehlke.com>
2021-10-04 16:02:28 +0200

    Remove unnecessary tailwind configs. Purge only when in prod mode.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,8 @@
 module.exports = {
-  prefix: '',
   purge: {
+    enabled: process.env.NODE_ENV === 'production',
     content: [
       './src/**/*.{html,ts}',
     ]
   },
-  darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [], // Add e.g. fonts or forms here
 };
