84ace269803e2e9b7581a50f91139577e8df0918
Moritz Eck <moritz.eck@gmail.com>
2021-10-04 18:57:42 +0200

    feat: add Dockerfile + move dependencies to single file

--- backend/src/rest/booksRouter.ts
+++ backend/src/rest/booksRouter.ts
@@ -1,4 +1,4 @@
-import { Router } from "https://deno.land/x/oak@v9.0.1/mod.ts";
+import { Router } from "../../dependencies.ts";
 import { books } from "../../data/mockdata.ts";
 
 const SUB_ROUTE = "/books";
