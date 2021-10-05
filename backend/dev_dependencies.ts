97e2c28174cc382ee719a4c3e0f5ef41cfe9d761
Moritz Eck <moritz.eck@gmail.com>
2021-10-05 11:14:48 +0200

    feat: add basic test

---
+++ backend/dev_dependencies.ts
@@ -0,0 +1,5 @@
+// import & re-rexport runtime dependencies
+export * from "./dependencies.ts";
+
+// dev dependencies
+export { assertEquals } from "https://deno.land/std@0.110.0/testing/asserts.ts";
