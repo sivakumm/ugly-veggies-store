430f3dbd1606c59ff356a527523fe32074de34b0
Lyndsey Bonelli <lyndsey.bonelli@zuehlke.com>
2021-10-06 15:14:28 +0200

    Add modules for abo and home.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,12 +8,12 @@ import {SharedModule} from "./shared/shared.module";
 import {CoreModule} from "./core/core.module";
 import {ProdukteModule} from "./produkte/produkte.module";
 import {RezepteModule} from "./rezepte/rezepte.module";
+import {HomeModule} from "./home/home.module";
+import {AbosModule} from "./abos/abos.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    AbosComponent,
-    HomeComponent,
     WarenkorbComponent,
   ],
   imports: [
@@ -21,8 +21,11 @@ import {RezepteModule} from "./rezepte/rezepte.module";
     CoreModule,
     ProdukteModule,
     RezepteModule,
+    HomeModule,
+    AbosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
