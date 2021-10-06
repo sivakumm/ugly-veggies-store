1f0ba15f2ab58b13a4e9f67ff493f577e5209799
Lyndsey Bonelli <lyndsey.bonelli@zuehlke.com>
2021-10-06 10:11:30 +0200

    Add own screen sizes to test custom themes.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   theme: {
     container: {
       padding: '1.25rem', // Alternatively set p-5 on the div container itself
-    }
+    },
+    screens: {
+      tablet: '640px',
+      laptop: '1024px',
+      desktop: '1280px',
+    },
   },
   mode: 'jit',
   purge: {
