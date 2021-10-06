5ce71283f5512adca484762700fe43de90c0d284
Moritz Eck <moritz.eck@gmail.com>
2021-10-06 13:51:59 +0200

    feat: add template benchmark

--- backend/dev_dependencies.ts
+++ backend/dev_dependencies.ts
@@ -3,3 +3,4 @@ export * from "./dependencies.ts";
 
 // dev dependencies
 export * as asserts from "https://deno.land/std@0.110.0/testing/asserts.ts";
+export { bench, runBenchmarks } from "https://deno.land/std@0.110.0/testing/bench.ts";
